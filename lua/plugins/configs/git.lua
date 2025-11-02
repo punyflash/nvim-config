ec5ef58ceecb0760c767daf707b634410083ec7f
Dmytro Morozov <puny.flash@gmail.com>
2025-11-02 22:23:08 +0200

    Add neogit

---
+++ lua/plugins/configs/git.lua
@@ -0,0 +1,16 @@
+return {
+    'NeogitOrg/neogit',
+    dependencies = {
+        'nvim-lua/plenary.nvim', -- required
+        'sindrets/diffview.nvim', -- optional - Diff integration
+
+        -- Only one of these is needed.
+        'nvim-telescope/telescope.nvim', -- optional
+        'ibhagwan/fzf-lua', -- optional
+        'nvim-mini/mini.pick', -- optional
+        'folke/snacks.nvim', -- optional
+    },
+    config = function()
+        require('neogit').setup {}
+    end,
+}
